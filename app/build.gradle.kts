cafc4411c2dd8da9a4886410bc47a8313e2512b9
amsyarasyiq <82711525+amsyarasyiq@users.noreply.github.com>
2025-08-01 18:25:01 +0800

    chore: bump version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.pyoncord.manager"
         minSdk = 28
         targetSdk = 35
-        versionCode = 1009
-        versionName = "1.0.9"
+        versionCode = 1100
+        versionName = "1.1.0"
 
         buildConfigField("String", "GIT_BRANCH", "\"${getCurrentBranch()}\"")
         buildConfigField("String", "GIT_COMMIT", "\"${getLatestCommit()}\"")
